2bb7648593406546fcdd3a03fab063b06cf05a5a
Your Name <you@example.com>
2018-11-15 15:31:14 +0800

    two-sided

--- Makefile
+++ Makefile
@@ -2,17 +2,28 @@ all: ps worker
 CC=g++
 TARGET = ps
 TARGET1 = worker
+TARGET2 = test_send
+TARGET3 = test_recv
 LIBS=-libverbs -lrdmacm -pthread -libverbs -lrdmacm
 CFLAGS=-Wall -g -fpermissive -std=c++11
 OBJS=ps.o server_rdma_op.o client_rdma_op.o rdma_common.o rdma_two_sided_client_op.o rdma_two_sided_server_op.o common.o
 OBJS1=worker.o server_rdma_op.o client_rdma_op.o rdma_common.o rdma_two_sided_client_op.o rdma_two_sided_server_op.o common.o
+OBJS2=test_send.o server_rdma_op.o client_rdma_op.o rdma_common.o rdma_two_sided_client_op.o rdma_two_sided_server_op.o common.o
+OBJS3=test_recv.o server_rdma_op.o client_rdma_op.o rdma_common.o rdma_two_sided_client_op.o rdma_two_sided_server_op.o common.o
 
 $(TARGET): $(OBJS)
 	$(CC) $(CFLAGS) -o $(TARGET) $(OBJS) $(LIBS)
 $(TARGET1): $(OBJS1)
 	$(CC) $(CFLAGS) -o $(TARGET1) $(OBJS1) $(LIBS)
+$(TARGET2): $(OBJS2)
+	$(CC) $(CFLAGS) -o $(TARGET1) $(OBJS1) $(LIBS)
+$(TARGET3): $(OBJS3)
+	$(CC) $(CFLAGS) -o $(TARGET1) $(OBJS1) $(LIBS)
 
-
+test_send.o test_send.cpp
+	$(CC) $(CFLAGS) -c test_send.cpp
+test_recv.o test_recv.cpp
+	$(CC) $(CFLAGS) -c test_recv.cpp
 worker.o: worker.cpp
 	$(CC) $(CFLAGS) -c worker.cpp
 ps.o: ps.cpp
---
+++ test_recv.cpp
@@ -0,0 +1,62 @@
+
+#include <iostream>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <signal.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <assert.h>
+#include <stdio.h>
+#include <string.h>
+#include <cmath>
+#include <time.h>
+#include <vector>
+#include <list>
+#include <thread>
+#include <chrono>
+#include <algorithm>
+#include <mutex>
+#include <atomic>
+#include <fstream>
+#include <sys/time.h>
+#include <map>
+#include "mf_common.h"
+#include "rdma_two_sided_client_op.h"
+#include "rdma_two_sided_server_op.h"
+using namespace std;
+
+#define RIP "12.12.10.3"
+#define LIP "12.12.11.3"
+#define RPORT 4444
+#define LPORT 5555
+
+void rdma_sendTd_loop();
+void rdma_recvTd_loop();
+struct client_context c_ctx;
+struct conn_context s_ctx;
+void rdma_sendTd_loop()
+{
+	char* remote_ip = RIP;
+	int remote_port = RPORT;
+	printf("remote_ip=%s  remote_port=%d\n", remote_ip, remote_port);
+	char str_port[100];
+	sprintf(str_port, "%d", remote_port);
+	RdmaTwoSidedClientOp ct;
+	ct.rc_client_loop(remote_ip, str_port, &(c_ctx));
+}
+
+void rdma_recvTd_loop()
+{
+	int bind_port =  LPORT;
+	char str_port[100];
+	sprintf(str_port, "%d", bind_port);
+	printf("bind_port = %d\n", bind_port );
+	RdmaTwoSidedServerOp rtos;
+	rtos.rc_server_loop(str_port, &(s_ctx));
+}
+
+int main(int argc, const char * argv[])
+{
+	rdma_recvTd_loop();
+}
