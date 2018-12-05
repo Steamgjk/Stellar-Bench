f338991f72c52ed81b058666459c8a0171c83edc
Your Name <you@example.com>
2018-12-05 16:49:52 +0800

    test makefile

--- gpu_mf.h
+++ gpu_mf.h
@@ -1,5 +1,4 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <cuda.h>
 #include "cuda_kernel_mf.h"
 void gpu_mf(double *row_eles, double* col_eles, int dim, int row_height, int col_height, double* rating_entries, int* random_row_id, int* random_col_id, int wn);
