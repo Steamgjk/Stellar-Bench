f771af1dca5a2cc57e5a6706761477bbc2b10699
Your Name <you@example.com>
2018-12-05 17:12:49 +0800

    test hybrid

--- gpu_mf.cu
+++ gpu_mf.cu
@@ -1,7 +1,3 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <cuda.h>
-#include <algorithm>
 #include "gpu_mf.h"
 using namespace std;
 void gpu_mf(double *row_eles, double* col_eles, int dim, int row_height, int col_height, double* rating_entries, int* random_row_id, int* random_col_id, int wn){
