49c84291abbd1a05bc8ac1540ffe9f51ac3dce35
Your Name <you@example.com>
2018-12-05 17:15:46 +0800

    test hybrid

--- cuda_kernel_mf.h
+++ cuda_kernel_mf.h
@@ -1,5 +1,8 @@
+#ifndef CUDA_KERNEL_H
+#define CUDA_KERNEL_H
 #include <stdio.h>
 #include <cuda.h>
 #include <stdlib.h>
 #include <algorithm>
 __global__ void cuda_mf(double *row_eles, double* col_eles, int dim, int row_height, int col_height, double* rating_entries, int* random_row_id, int* random_col_id, int wn);
+#endif
--- gpu_mf.h
+++ gpu_mf.h
@@ -1,2 +1,5 @@
+#ifndef GPU_MF_H
+#define GPU_MF_H
 #include "cuda_kernel_mf.h"
 void gpu_mf(double *row_eles, double* col_eles, int dim, int row_height, int col_height, double* rating_entries, int* random_row_id, int* random_col_id, int wn);
+#endif
