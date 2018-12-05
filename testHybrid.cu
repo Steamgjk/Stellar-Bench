0d0c24f07a31c0ac66a507e7e0ab26f397b9160a
Your Name <you@example.com>
2018-12-05 20:14:18 +0800

    ok

--- testHybrid.cu
+++ testHybrid.cu
@@ -1,7 +1,29 @@
-#include "gpu_mf.h"
-#include <stdio.h>
+#include <iostream>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <signal.h>
+#include <unistd.h>
 #include <stdlib.h>
+#include <assert.h>
+#include <stdio.h>
+#include <string.h>
+#include <cmath>
+#include <time.h>
+#include <vector>
+#include <list>
 #include <thread>
+#include <chrono>
+#include <algorithm>
+#include <mutex>
+#include <iostream>
+#include <fstream>
+#include <sys/time.h>
+#include <map>
+#include "mf_common.h"
+#include "gpu_mf.h"
+#include "rdma_two_sided_client_op.h"
+#include "rdma_two_sided_server_op.h"
 using namespace std;
 void rdma_recvTd_loop(int id)
 {
